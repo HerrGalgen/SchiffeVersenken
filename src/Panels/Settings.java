8578bff061961d8f70644beef49bf7b4e9770bb4
HerrGalgen <kreuzer.fabian4@gmail.com>
2022-04-20 11:27:37 +0200

    avoid low or high inputs in Settings

--- src/Panels/Settings.java
+++ src/Panels/Settings.java
@@ -1,30 +1,32 @@
 package Panels;
 
 import GameIO.PropertyReader;
+import Summaries.GameMain;
 import Summaries.GameSummary;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.util.Map;
 import java.util.Properties;
 
 public class Settings extends JFrame {
 
-    GameSummary gameSummary;
-    PropertyReader reader = new PropertyReader();
-    Properties properties = reader.getProperties();
+    private final PropertyReader reader = new PropertyReader();
+    private final Properties properties = reader.getProperties();
 
-    String[] names = new String[properties.size()];
-    String[] values = new String[properties.size()];
+    private final JCheckBox cbrestart = new JCheckBox("Spiel beim verlassen neu starten");
+    private final String[] gameSize = {"700", "800", "900", "1000"};
+    private final String[] tableSize = {"10", "11", "12", "13", "14", "15", "16", "16", "17"};
 
-    JLabel[] labelNames = new JLabel[properties.size()];
-    JTextField[] fieldValues = new JTextField[properties.size()];
+    private final JComboBox<String> cbGameSizeX = new JComboBox<>(gameSize);
+    private final JComboBox<String> cbTableSizeX = new JComboBox<>(tableSize);
+    private final JComboBox<String> cbGameSizeY = new JComboBox<>(gameSize);
+    private final JComboBox<String> cbTableSizeY = new JComboBox<>(tableSize);
+
 
     public Settings(GameSummary gameSummary) {
 
-        this.gameSummary = gameSummary;
         setVisible(true);
         setMinimumSize(new Dimension(gameSummary.getWidth(), gameSummary.getHeight()));
         setLayout(new GridBagLayout());
@@ -32,41 +34,7 @@ public class Settings extends JFrame {
 
         gameSummary.setSettingsOpen(true);
 
-        GridBagConstraints cons = new GridBagConstraints();
-
-        int i = 0;
-
-        for(Map.Entry<Object, Object> entry : properties.entrySet()) {
-            names[i] = (String) entry.getKey();
-            values[i] = (String) entry.getValue();
-            i++;
-        }
-
-        cons.gridx = 0;
-        cons.gridy = 0;
-        cons.insets = new Insets(5,10,5,10);
-
-        for(int index = 0; index < properties.size(); index++) {
-            //add labels for Settings:
-            cons.gridx = 0;
-            labelNames[index] = new JLabel(names[index]);
-            labelNames[index].setFont(new Font("Dialog", Font.PLAIN, 20));
-            labelNames[index].setAlignmentX(JLabel.LEFT_ALIGNMENT);
-            add(labelNames[index], cons);
-
-            cons.gridx++;
-
-            //Add Text-Boxes for Settings:
-            fieldValues[index] = new JTextField(values[index]);
-            fieldValues[index].setFont(new Font("Dialog", Font.PLAIN, 20));
-            fieldValues[index].setAlignmentX(JLabel.LEFT_ALIGNMENT);
-            fieldValues[index].setPreferredSize(new Dimension(80,30));
-            add(fieldValues[index], cons);
-            cons.gridy++;
-        }
-
-        cons.gridx++;
-        cons.gridy = 0;
+        setupSettings();
 
         addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
@@ -74,19 +42,65 @@ public class Settings extends JFrame {
                 reader.setProperties(properties);
                 System.out.println("----[ Einstellungen geschlossen ]-----");
                 gameSummary.setSettingsOpen(false);
+                if (cbrestart.isSelected()) {
+                    gameSummary.dispose();
+                    GameMain.main(new String[2]);
+                }
             }
         });
 
         System.out.println("----[ Einstellungen erscheinen ]-----");
     }
 
-    private void getNewProperties() {
-        int i = 0;
-        for(JTextField field : fieldValues) {
-            properties.setProperty(names[i], field.getText());
-            i++;
-        }
+    private void setupSettings() {
+
+        cbGameSizeX.setPreferredSize(new Dimension(60, 20));
+        cbGameSizeY.setPreferredSize(new Dimension(60, 20));
+        cbTableSizeX.setPreferredSize(new Dimension(60, 20));
+        cbTableSizeY.setPreferredSize(new Dimension(60, 20));
+
+        ((JLabel)cbGameSizeX.getRenderer()).setHorizontalAlignment(SwingConstants.CENTER);
+        ((JLabel)cbGameSizeY.getRenderer()).setHorizontalAlignment(SwingConstants.CENTER);
+        ((JLabel)cbTableSizeX.getRenderer()).setHorizontalAlignment(SwingConstants.CENTER);
+        ((JLabel)cbTableSizeY.getRenderer()).setHorizontalAlignment(SwingConstants.CENTER);
+
+        cbGameSizeY.setSelectedItem(properties.getProperty("gridy"));
+        cbTableSizeX.setSelectedItem(properties.getProperty("gridx"));
+        cbGameSizeX.setSelectedItem(properties.getProperty("width"));
+        cbGameSizeY.setSelectedItem(properties.getProperty("height"));
+
+        GridBagConstraints cons = new GridBagConstraints();
+        cons.gridy = 0;
+        cons.gridx = 0;
+        cons.insets = new Insets(5, 10, 5, 10);
+        //Tabellengröße:
+        add(new JLabel("Tabelle: "), cons);
+        cons.gridx++;
+        add(cbTableSizeY, cons);
+        cons.gridx++;
+        add(cbTableSizeX, cons);
+
+
+        //Spielfeldgröße:
+        cons.gridx = 0;
+        cons.gridy++;
+        add(new JLabel("Spielfeld:"), cons);
+        cons.gridx++;
+        add(cbGameSizeY, cons);
+        cons.gridx++;
+        add(cbGameSizeX, cons);
+
+        //Restart on close:
+        cons.gridy++;
+        cons.gridx = 0;
+        add(cbrestart, cons);
     }
 
+    private void getNewProperties() {
+        properties.setProperty("gridx", (String) cbTableSizeX.getSelectedItem());
+        properties.setProperty("gridy", (String) cbTableSizeY.getSelectedItem());
+        properties.setProperty("width", (String) cbGameSizeX.getSelectedItem());
+        properties.setProperty("height", (String) cbGameSizeY.getSelectedItem());
 
+    }
 }
